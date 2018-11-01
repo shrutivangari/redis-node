585c2df87ec11441d3f2fab5e6209ef451d10a06
Vangari, Shruti <Shruti.Vangari@nordstrom.com>
2018-10-31 18:11:19 -0700

    Bitmap update

--- concepts/bitmap.js
+++ concepts/bitmap.js
@@ -23,5 +23,19 @@ function countVisits(date, client) {
 
 function showUserIdsFromVisit(date, client) {
     var key='visits:daily:'+date;
-    client.get(key, function(err, bitmapValue))
+    client.get(key, function(err, bitmapValue) {
+        var userIds=[];
+        var data = bitmapValue.toJSON().data;
+
+        data.forEach(function(byte, byteIndex) {
+            for(var bitIndex=7; bitIndex>=0; bitIndex --) {
+                var visited = byte >> bitIndex &1;
+                if(visited === 1) {
+                    var userId = byteIndex * 8 + (7 - bitIndex);
+                    userIds.push(userId);
+                }
+            }
+        });
+        console.log("Users " + userIds + " visited on " + date);
+    });
 }
