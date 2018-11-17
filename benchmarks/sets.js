0277ca0c2b83f50b25f3a17c546bf7ae3acdeba5
Vangari, Shruti <Shruti.Vangari@nordstrom.com>
2018-11-16 18:30:20 -0800

    Benchmarks

---
+++ benchmarks/sets.js
@@ -0,0 +1,17 @@
+function benchmarkSets(client) {
+    var MAX_USERS = 100000;
+    var MAX_DEALS = 12;
+    var MAX_DEAL_ID = 10000;
+
+    for(var i=0; i< MAX_USERS; i++) {
+        var multi = client.multi();
+        for(var j=0; j<MAX_DEALS; j++) {
+            multi.sadd("set:user:" + i, MAX_DEAL_ID -j, 1);
+        }
+        multi.exec();
+    }
+}
+
+module.exports = {
+    benchmarkSets:benchmarkSets
+}
