4172bae0fce34239deb39c65089cfec5958e8e06
Vangari, Shruti <Shruti.Vangari@nordstrom.com>
2018-11-19 14:13:47 -0800

    Benchmark for bitmaps

--- benchmarks.js
+++ benchmarks.js
@@ -3,7 +3,42 @@
  */
 var redis = require("redis");
 var sets = require("./benchmarks/sets");
-var client = redis.createClient();
+var bitmaps = require("./benchmarks/bitmaps");
 
+
+/**
+ * # Memory
+ used_memory:14860000
+ used_memory_human:14.17M
+ used_memory_rss:147779584
+ used_memory_peak:2542119984
+ used_memory_peak_human:2.37G
+ used_memory_lua:35840
+ mem_fragmentation_ratio:9.94
+ mem_allocator:libc
+
+ */
+var client = redis.createClient();
 sets.benchmarkSets(client);
 client.quit();
+
+
+/**
+ * # Memory
+ used_memory:266060416
+ used_memory_human:253.73M
+ used_memory_rss:337969152
+ used_memory_peak:2542119984
+ used_memory_peak_human:2.37G
+ used_memory_lua:35840
+ mem_fragmentation_ratio:1.27
+ mem_allocator:libc
+
+ */
+var client = redis.createClient();
+bitmaps.benchmarkBitmaps(client);
+client.quit();
+
+/**
+ The Bitmap implementation used approximately 253 MB while the Set implementation used approximately 14 MB. The Bitmap implementation (which was supposed to be cheaper) costs 18 times more in the scenario described earlier.
+ */
